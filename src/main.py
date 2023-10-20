21cc97059dca77cd3d5e9fdd6c2e2c1018adc5e6
Rami <rshaar559@gmail.com>
2023-10-20 16:34:53 +0300

    Added matplotlib support

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from widgets import win
 from gui_layout import init_gui_layout, assign_gui_funcs
+import matplotlib
 
-
+matplotlib.use('TkAgg')
 init_gui_layout()
 assign_gui_funcs()
 win.mainloop()
